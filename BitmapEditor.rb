053319bfdd85c04799ba1cb2bb1b097fb88461ab
talhabutt7 <talhabuttt@gmail.com>
2022-05-23 05:49:01 +0100

    Functions in BitmapEditor return output instead of using 'puts'. No utility functions can print to the console, allows for testing

--- BitmapEditor.rb
+++ BitmapEditor.rb
@@ -1,15 +1,12 @@
 class BitmapEditor
-
   # Constants
   BLANK_PIXEL = 'O'
   MAX_SIZE = 250
-
   # Initialise the class as the program hasn't started yet
   @running = false
   @image = nil
   @width = 0
   @height = 0
-
   # Start the program running
   # @public
   # @return void
@@ -18,224 +15,238 @@ class BitmapEditor
     @running = true
     puts 'Welcome to Bitmap Editor'
     puts 'type ? for help'
+
     # Keep looping until the user ends the program
     while @running
 
       print '> '
 
-      # Get the input from the user and switch on the first character
+      # Get the input from the user
       input = gets.chomp
-      first_char = input[0, 1].capitalize
-      # Call a utiltity function to break down the white-spaced inputs
-      # into an array of strings
-      args = Utils.analyse_input(input)
-      # Check the correct number of arguments
-      if Utils.number_args(first_char, args)
-
-        case first_char
-        when '?'
-          show_help
-        when 'I'
-          new_image(args)
-        when 'S'
-          show_image
-        when 'C'
-          clear_image
-        when 'L'
-          set_pixel_colour(args)
-        when 'H', 'V'
-          draw_line(first_char, args)
-        when 'X'
-          exit_console
-        else
-          puts 'Unrecognised command "' + input + '"'
-        end
-
+      output = call(input)
+      if (output != "")
+        puts output
       end
 
     end
+
   end
+
+  # Make a call to the private functions below based on user input
+  # and returns the result to print to the screen
+  # @public
+  # @param input - The unformatted raw input string
+  # @return string
+  def call(input)
+
+    first_char = input[0, 1].capitalize
+
+    # Call a utiltity function to break down the white-spaced inputs
+    # into an array of strings
+    args = Utils.analyse_input(input)
+
+    # Check the correct number of arguments
+    check_args = Utils.number_args(first_char, args)
+    return check_args if check_args != ""
+
+    case first_char
+    when '?'
+      return show_help
+    when 'I'
+      return new_image(args)
+    when 'S'
+      return show_image
+    when 'C'
+      return clear_image
+    when 'L'
+      return set_pixel_colour(args)
+    when 'H', 'V'
+      return draw_line(first_char, args)
+    when 'X'
+      return exit_console
+    else
+      return 'Unrecognised command "' + input + '"'
+    end
+
+  end
+
   private
 
   # Create a new bitmap in the class
   # @private
   # @param args - The array of string arguments
-  # @return void
+  # @return string
   def new_image(args)
 
     # Convert the width and height arguments to integer
     @width = args[0].to_i
     @height = args[1].to_i
 
-    # minimum size of the canvas
     if @width < 1 || @height < 1
-      puts "The image must be at minimum 1x1"
-      return
+      return "The image must be at minimum 1x1"
     end
 
-    # maximum size of the canvas
     if @width > MAX_SIZE || @height > MAX_SIZE
-      puts "The maximum width and height is " + MAX_SIZE.to_s
-      return
+      return "The maximum width and height is " + MAX_SIZE.to_s
     end
 
     # Create a new 2-dimensional array
     # and clear the image
     @image = Array.new(@height) { Array.new(@width) }
-    clear_image
+    return clear_image
 
   end
+
   # Replaces all the pixels in the image by an O
   # @private
-  # @return void
+  # @return string
   def clear_image
-    if image_created
-      for y in 0..(@height-1)
-        for x in 0..(@width-1)
-          @image[y][x] = BLANK_PIXEL
-        end
+    return image_created if image_created != ""
+    for y in 0..(@height-1)
+      for x in 0..(@width-1)
+        @image[y][x] = BLANK_PIXEL
       end
     end
+    return ""
   end
 
   # Sets the colour of one pixel
   # @param args - The array containing the X, Y and colour
-  # @return void
+  # @return string
   def set_pixel_colour(args)
 
-    if image_created
+    return image_created if image_created != ""
 
-      # x and y must be numbers
-      if !Utils.is_numeric(args[0]) || !Utils.is_numeric(args[1])
-        puts "X and Y must both be numbers"
-        return
-      end
-
-      # Get the integer values of X and Y
-      # and subtract 1 so that the first pixel is (1, 1)
-      x = args[0].to_i - 1
-      y = args[1].to_i - 1
-
-      # Must be a valid colour
-      col = args[2]
-      if !Utils.is_valid_colour(col)
-        puts "Colours must be capital letters"
-        return
-      end
-
-      # x and y must be valid bounds within the image
-      if Utils.is_bounded(x, 0, @width - 1) &&
-        Utils.is_bounded(y, 0, @height - 1)
-        @image[y][x] = col
-      end
-
-      @image[y][x] = col
-
+    # x and y must be numbers
+    if !Utils.is_numeric(args[0]) ||
+      !Utils.is_numeric(args[1])
+      return "X and Y must both be numbers"
     end
+
+    # Get the integer values of X and Y
+    # and subtract 1 so that the first pixel is (1, 1)
+    x = args[0].to_i - 1
+    y = args[1].to_i - 1
+
+    # Must be a valid colour
+    col = args[2]
+    if !Utils.is_valid_colour(col)
+      return "Colours must be capital letters"
+    end
+
+    # x and y must be valid bounds within the image
+    if Utils.is_bounded(x, 0, @width - 1) &&
+      Utils.is_bounded(y, 0, @height - 1)
+      @image[y][x] = col
+    else
+      return "You entered a pixel which isn't in the image"
+    end
+
+    return ""
+
   end
 
   def draw_line(type, args)
 
-    if image_created
+    return image_created if image_created != ""
 
-      # x and y values must be numbers
-      if !Utils.is_numeric(args[0]) ||
-        !Utils.is_numeric(args[1]) ||
-        !Utils.is_numeric(args[2])
-        puts "X and Y values must be numbers"
-        return
-      end
+    # x and y values must be numbers
+    if !Utils.is_numeric(args[0]) ||
+      !Utils.is_numeric(args[1]) ||
+      !Utils.is_numeric(args[2])
+      return "X and Y values must be numbers"
+    end
 
-      # Must be a valid colour
-      col = args[3]
-      if !Utils.is_valid_colour(col)
-        puts "Colours must be capital letters"
-        return
-      end
+    # Must be a valid colour
+    col = args[3]
+    if !Utils.is_valid_colour(col)
+      return "Colours must be capital letters"
+    end
 
-      # Generic variable names as we are covering for both
-      # H and V in this function.
-      # Again, subtract 1 so that the first pixel is (1, 1)
-      num1 = args[0].to_i - 1
-      num2 = args[1].to_i - 1
-      num3 = args[2].to_i - 1
+    # Generic variable names as we are covering for both
+    # H and V in this function.
+    # Again, subtract 1 so that the first pixel is (1, 1)
+    num1 = args[0].to_i - 1
+    num2 = args[1].to_i - 1
+    num3 = args[2].to_i - 1
 
-      for y in 0..(@height-1)
-        for x in 0..(@width-1)
+    for y in 0..(@height-1)
+      for x in 0..(@width-1)
 
-          if type == "V"
-            if Utils.is_bounded(num1, 0, @width - 1) &&
-              Utils.is_bounded(num2, 0, @height - 1) &&
-              Utils.is_bounded(num3, 0, @height - 1)
-              if x == num1 && y >= num2 && y <= num3
-                @image[y][x] = col
-              end
-            else
-              return
+        if type == "V"
+          if Utils.is_bounded(num1, 0, @width - 1) &&
+            Utils.is_bounded(num2, 0, @height - 1) &&
+            Utils.is_bounded(num3, 0, @height - 1)
+            if x == num1 && y >= num2 && y <= num3
+              @image[y][x] = col
             end
-          elsif type == "H"
-            if Utils.is_bounded(num1, 0, @width - 1) &&
-              Utils.is_bounded(num2, 0, @width - 1) &&
-              Utils.is_bounded(num3, 0, @height - 1)
-              if x >= num1 && x <= num2 && y == num3
-                @image[y][x] = col
-              end
-            else
-              return
-            end
+          else
+            return "You entered a pixel which isn't in the image"
           end
-
+        elsif type == "H"
+          if Utils.is_bounded(num1, 0, @width - 1) &&
+            Utils.is_bounded(num2, 0, @width - 1) &&
+            Utils.is_bounded(num3, 0, @height - 1)
+            if x >= num1 && x <= num2 && y == num3
+              @image[y][x] = col
+            end
+          else
+            return "You entered a pixel which isn't in the image"
+          end
         end
+
       end
     end
-  end
 
+    return ""
+
+  end
 
   # Prints the image to the screen
   # @private
-  # @return void
+  # @return string
   def show_image
-    if image_created
-
-      for y in 0..(@image.length-1)
-        for x in 0..(@image[y].length-1)
-          print @image[y][x]
-        end
-        print "\n"
+    return image_created if image_created != ""
+    output = ""
+    for y in 0..(@image.length-1)
+      for x in 0..(@image[y].length-1)
+        output += @image[y][x]
       end
+      output += "\n"
     end
+    return output.strip
   end
 
   # Check that an image has been created
   # @private
-  # @return boolean
+  # @return string
   def image_created
     if @image == nil
-      puts "You haven't created an image yet"
-      return false
+      return "You haven't created an image yet"
     else
-      return true
+      return ""
     end
   end
 
-
   # Exits the program
   # @private
-  # @return void
+  # @return string
   def exit_console
-    puts "Thanks for running the program!"
     @running = false
+    return "Thanks for running the program!"
   end
+
   # Prints help text
   # @private
-  # @return void
+  # @return string
   def show_help
-    puts "I M N - Create a new M x N canvas. \n" +
-           "C - Sets all pixels to colour '" + BLANK_PIXEL + "'. \n" +
-           "L X Y C - Colours the pixel (X,Y) with colour C. \n" +
-           "V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive). \n" +
-           "H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive). \n" +
-           "S - Show the contents of the current canvas \n" +
-           "X - Terminate the session"
+    return "I M N - Create a new M x N canvas. \n" +
+      "C - Sets all pixels to colour '" + BLANK_PIXEL + "'. \n" +
+      "L X Y C - Colours the pixel (X,Y) with colour C. \n" +
+      "V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive). \n" +
+      "H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive). \n" +
+      "S - Show the contents of the current canvas \n" +
+      "X - Terminate the session"
   end
+
 end
--- Utils.rb
+++ Utils.rb
@@ -12,12 +12,11 @@ class Utils
 
     # Break up the string into an array
     parts = clean.split(" ")
+
     # We no longer need the first argument
     parts.shift
     return parts
-
   end
-
   # Returns whether or not the given string is a capital letter
   # @static
   # @param col - The string entered as the colour
@@ -25,8 +24,6 @@ class Utils
   def self.is_valid_colour(col)
     return /[[:upper:]]/.match(col)
   end
-  
-
   # Returns whether or not the given string is a numeric
   # @static
   # @param val - The value entered as the number
@@ -34,7 +31,6 @@ class Utils
   def self.is_numeric(val)
     return val.to_f.to_s == val.to_s || val.to_i.to_s == val.to_s
   end
-
   # Checks whether or not the given index is correctly bounded
   # within the constraints
   # @static
@@ -44,7 +40,6 @@ class Utils
   # @return boolean
   def self.is_bounded(index, min, max)
     if index < min || index > max
-      puts "You entered a pixel which isn't in the image"
       return false
     else
       return true
@@ -66,12 +61,11 @@ class Utils
 
     # Only worry if expected_args > 0 and it doesn't match
     if expected_args > 0 && args.length != expected_args
-      puts "You should only enter " + expected_args.to_s +
-             " value(s) after the '" + cmd + "'"
-      return false
+      return "You should only enter " + expected_args.to_s +
+        " value(s) after the '" + cmd + "'"
     end
 
-    return true
+    return ""
 
   end
 
